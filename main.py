b25b52296ed48f80c3600a28c2c0f8cd93d414b6
steve-kim-96 <soungku@hotmail.com>
2025-03-09 19:31:30 +1300

    clean up printing as per lesson and use sys argv instead of hard-coded file path

--- main.py
+++ main.py
@@ -1,1 +1,27 @@
-print('Greetings boots')
+from stats import *
+import sys
+
+def get_book_text (path_to_file):
+    with open(path_to_file) as f:
+        return f.read()
+    
+    
+def main ():
+    if len(sys.argv) < 2:
+        print("Usage: python3 main.py <path_to_book>")
+        sys.exit(1)
+    book_text = get_book_text(sys.argv[1])
+    num_words = get_character_count(book_text)
+    character_count = sort_character_count(num_words)
+    print("============ BOOKBOT ============")
+    print("Analyzing book found at books/frankenstein.txt...")
+    print("----------- Word Count ----------")
+    print("Found 75767 total words")
+    print("--------- Character Count -------")
+    for count_dict in character_count:
+        if count_dict["character"].isalpha():
+            print(f"{count_dict["character"]}: {count_dict["num"]}")
+
+    print("============= END ===============")
+
+main()
